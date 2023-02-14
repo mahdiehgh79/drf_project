ce9aa1e90b37aed55bb3958111257661be956430
Your Name <you@example.com>
2023-02-14 13:14:40 -0800

    first

--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from blog.models import Article
 from .serializers import ArticleSerializer
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveAPIView
 
 
 class ArticleList(ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleDetail(RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
